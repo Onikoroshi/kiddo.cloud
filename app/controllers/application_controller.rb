5a89622bc1b577258ccc4a625fe731b8b95ddf76
Jason Eastwood <jeastwood@gmail.com>
2017-05-26 12:51:25 -0700

    Basic multi tenancy functionality via accounts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :set_account
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def set_account
+    @account = Account.find_by(subdomain: request.subdomain) if (request.subdomain.present? && !["www", "admin"].include?(request.subdomain))
+  end
+
+  # Add to any controller to require multitenancy. You would leave this out of controllers that manage pages like "team"
+  # or "about": pages that pertain to the whole application and not just a single account.
+  def require_account!
+    redirect_to root_url(subdomain: "wwww") if !@account.present?
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation, account_attributes: [:subdomain]])
+  end
+
 end
