56e99317b6a88213193327f9f9b030ac09d1a41f
Jason Eastwood <jeastwood@gmail.com>
2017-05-29 11:33:10 -0700

    Added emergency contacts

--- app/models/core.rb
+++ app/models/core.rb
@@ -1,4 +1,5 @@
 class Core < ApplicationRecord
   belongs_to :center
   has_many :parents
+  has_many :emergency_contacts, dependent: :destroy
 end
---
+++ db/migrate/20170529182616_create_emergency_contacts.rb
@@ -0,0 +1,12 @@
+class CreateEmergencyContacts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :emergency_contacts do |t|
+      t.references :core, foreign_key: true
+      t.string :first_name
+      t.string :last_name
+      t.string :phone
+
+      t.timestamps
+    end
+  end
+end
