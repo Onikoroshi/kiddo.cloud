b0c666cedcfe68b90d0f4f83ffbbd09cf2b9100c
Jason Eastwood <jeastwood@gmail.com>
2017-06-09 12:13:47 -0700

    Seed locations

---
+++ db/fixtures/05_center_locations.rb
@@ -0,0 +1,61 @@
+Location.seed_once(:name) do |l|
+  l.center_id = 2
+  l.name = "Birch Lane"
+end
+
+Location.seed_once(:name) do |l|
+  l.center_id = 2
+  l.name = "Pine Avenue"
+end
+
+Location.seed_once(:name) do |l|
+  l.center_id = 2
+  l.name = "Hickory Street"
+end
+
+Location.seed_once(:name) do |l|
+  l.center_id = 2
+  l.name = "Maple Circle"
+end
+
+Location.seed_once(:name) do |l|
+  l.center_id = 3
+  l.name = "Chinchilla Lane"
+end
+
+Location.seed_once(:name) do |l|
+  l.center_id = 3
+  l.name = "Sable Avenue"
+end
+
+Location.seed_once(:name) do |l|
+  l.center_id = 3
+  l.name = "Hotot Street"
+end
+
+Location.seed_once(:name) do |l|
+  l.center_id = 3
+  l.name = "Wolly Circle"
+end
+
+
+Location.seed_once(:name) do |l|
+  l.center_id = 4
+  l.name = "Lake Avenue"
+end
+
+Location.seed_once(:name) do |l|
+  l.center_id = 4
+  l.name = "Twin View"
+end
+
+Location.seed_once(:name) do |l|
+  l.center_id = 4
+  l.name = "Collegeview"
+end
+
+Location.seed_once(:name) do |l|
+  l.center_id = 4
+  l.name = "Caterpillar"
+end
+
