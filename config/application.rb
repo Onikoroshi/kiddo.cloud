21b621013764157df7f867761e0080911efb2f68
Jason Eastwood <jeastwood@gmail.com>
2017-08-08 22:05:19 -0700

    Config autoload entire lib dir

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,8 @@ module Kidsclub
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.autoload_paths += %W(#{config.root}/lib)
+    #config.autoload_paths += %W(#{config.root}/lib)
+    config.autoload_paths += Dir["#{config.root}/lib/**/"]
     config.autoload_paths += Dir[Rails.root.join('app', 'models', '**/')]
   end
 end
