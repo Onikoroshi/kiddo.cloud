c718ea59ba01714324079f13d075c99ca12023b0
Jason Eastwood <jeastwood@gmail.com>
2017-08-09 16:05:33 -0700

    Data manipulation

--- lib/maintenance_tools/production_seeder.rb
+++ lib/maintenance_tools/production_seeder.rb
@@ -14,8 +14,12 @@ class MaintenanceTools::ProductionSeeder
   def seed_all
     begin
       @center = Center.where(name: "Davis Kids Klub", subdomain: "daviskidsklub").first_or_create!
+      Center.where(name: "Kiddo.Cloud", subdomain: "www").first_or_create!
       @program = seed_program
       seed_program_plans
+      seed_roles
+      seed_users
+      seed_locations
     rescue => e
       puts e.message
       puts e.backtrace
@@ -45,6 +49,52 @@ class MaintenanceTools::ProductionSeeder
     Plan.where(program: program, short_code: "drop_1_wed", display_name: "Daily drop-in per day (Wednesday)", days_per_week: 0, price: 35.00, plan_type: "drop-in").first_or_create!
   end
 
+  def seed_roles
+    Role.where(name: "super_admin").first_or_create!
+    Role.where(name: "director").first_or_create!
+    Role.where(name: "staff").first_or_create!
+    Role.where(name: "parent").first_or_create!
+  end
+
+  def seed_users
+    User.create(
+      email: "daviskidsklub@aol.com",
+      first_name: "Lynda",
+      last_name: "Yancher",
+      roles: [Role.find_by(name: "director")],
+      center: Center.find_by(subdomain: "daviskidsklub")
+    )
+
+    User.create(
+      email: "jason@codinglabs.com",
+      first_name: "Jason",
+      last_name: "Eastwood",
+      roles: Role.all,
+      center: Center.find_by(subdomain: "www")
+    )
+
+    User.create(
+      email: "ian.kilpatrick@gmail.com",
+      first_name: "Ian",
+      last_name: "kilpatrick",
+      roles: Role.all,
+      center: Center.find_by(subdomain: "www")
+    )
+
+    User.all.map { |u| u.update_attributes!(password: "asdfasdf") }
+  end
+
+  def seed_locations
+    Location.where(name: "Birch Lane Elementary", center: center).first_or_create!
+    Location.where(name: "Korematsu Elementary", center: center).first_or_create!
+    Location.where(name: "M. Montgomery Elementary", center: center).first_or_create!
+    Location.where(name: "Patwin Elementary", center: center).first_or_create!
+    Location.where(name: "Pioneer Elementary", center: center).first_or_create!
+    Location.where(name: "Willett Elementary", center: center).first_or_create!
+    Location.where(name: "North Davis School", center: center).first_or_create!
+  end
+
+
   # def seed_stripe
   #   program.plans.each do |p|
   #     begin
@@ -63,8 +113,14 @@ class MaintenanceTools::ProductionSeeder
   # end
 
   def backout
-    Program.first.destroy
+    DropIn.destroy_all
+    Enrollment.destroy_all
     Plan.destroy_all
+    Program.first.destroy
+    Center.destroy_all
+    User.destroy_all
+    Role.destroy_all
+    Location.destroy_all
   end
 
 end
