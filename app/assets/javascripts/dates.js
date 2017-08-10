2574f2e21fdb5754a8a0647a4bf1fe8c65418131
Jason Eastwood <jeastwood@gmail.com>
2017-08-09 18:13:01 -0700

    Birthdate picker set default date instead of max date
    + friendly alt input

--- app/assets/javascripts/dates.js
+++ app/assets/javascripts/dates.js
@@ -2,11 +2,13 @@ document.addEventListener("turbolinks:load", function() {
   Turbolinks.clearCache()
   flatpickr('#flatpickr-dob-input', {
     dateFormat: "Y-m-d",
-    maxDate: "1-1-2005"
+    defaultDate: "1-1-2007",
+    altInput: true
   });
 
   flatpickr('#flatpickr-input', {
     dateFormat: "Y-m-d",
+    altInput: true
   });
 
 });
