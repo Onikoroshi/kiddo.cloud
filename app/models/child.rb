2948d25376476ab216d75bae0a01d5a5debc7ac1
Jason Eastwood <jeastwood@gmail.com>
2017-08-14 23:32:50 -0700

    Fixed child attendance tracking bug

--- app/models/child.rb
+++ app/models/child.rb
@@ -48,4 +48,13 @@ class Child < ApplicationRecord
     self.enrollments.where(plan: program.plans).present?
   end
 
+  def last_time_entry
+    self.time_entries.last
+  end
+
+  def on_clock?
+    return false unless last_time_entry.present?
+    last_time_entry.record_type == "entry"
+  end
+
 end
