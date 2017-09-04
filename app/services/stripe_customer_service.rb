9928481d9e728719921b39f39b6eb7cce6901740
Jason Eastwood <jeastwood@gmail.com>
2017-09-04 12:00:21 -0700

    Ensure we don't get duplicate customers

--- app/services/stripe_customer_service.rb
+++ app/services/stripe_customer_service.rb
@@ -1,21 +1,21 @@
 class StripeCustomerService
-
   attr_reader :account
   def initialize(account)
     @account = account
   end
 
   def find_or_create_customer(token)
+    customer = nil
     if account.customer?
-      Stripe::Customer.retrieve(account.gateway_customer_id)
+      customer = Stripe::Customer.retrieve(account.gateway_customer_id)
     else
-      Stripe::Customer.create(email: account.primary_email, source: token)
-      # create and save gateway id to account
+      customer = Stripe::Customer.create(email: account.primary_email, source: token)
+      account.update_attributes(gateway_customer_id: customer.id)
     end
+    customer
   end
 
   def find_customer
     Stripe::Customer.retrieve(account.gateway_customer_id)
   end
-
 end
