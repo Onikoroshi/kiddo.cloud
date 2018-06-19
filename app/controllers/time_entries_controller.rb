1bc4e43ff40e4c22fec1f99a1215e87a2a89f063
Peter T Cormack <petertcormack@gmail.com>
2018-06-19 15:00:47 -0700

    fix checking in children

--- app/controllers/time_entries_controller.rb
+++ app/controllers/time_entries_controller.rb
@@ -3,14 +3,19 @@ class TimeEntriesController < ApplicationController
   # POST /time_entries
   def create
     respond_to do |format|
-      found_staff = Staff.find_by(pin_number: params["pin_number"])
+      if params["time_entry"]["time_recordable_type"] == "Staff"
+        found_staff = Staff.find_by(pin_number: params["pin_number"])
 
-      if found_staff.present?
+        if found_staff.present?
+          record_type = params["on_clock"].present? ? TimeEntryType[:entry] : TimeEntryType[:exit]
+          @time_entry = TimeEntry.new(time_entry_params.merge({ time: Time.zone.now, record_type: record_type }))
+        end
+      elsif params["time_entry"]["time_recordable_type"] == "Child"
         record_type = params["on_clock"].present? ? TimeEntryType[:entry] : TimeEntryType[:exit]
         @time_entry = TimeEntry.new(time_entry_params.merge({ time: Time.zone.now, record_type: record_type }))
       end
 
-      if @time_entry.present? && @time_entry.save
+      if @time_entry.present? && @time_entry.save!
         format.js { render json: Hash.new }
       else
         format.json { render json: @time_entry.present? ? @time_entry.errors : "PIN Number Incorrect", status: :unprocessable_entity }
