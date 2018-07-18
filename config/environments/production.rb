8a5178812a6a8dcb09ff6e0769c7a1cad4101e48
Peter Cormack <petertcormack@gmail.com>
2018-07-18 13:11:29 -0700

    try serving static files instead

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,8 +22,10 @@ Rails.application.configure do
   config.assets.js_compressor = :uglifier
   config.assets.css_compressor = :sass
 
-  # Do fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  # Do not fallback to assets pipeline if a precompiled asset is missed.
+  config.assets.compile = false
+
+  config.serve_static_files = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
