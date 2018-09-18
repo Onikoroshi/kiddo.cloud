1047918432f9a1c4db8b2ad262b2ff54fb8fc90e
Peter Cormack <petertcormack@gmail.com>
2018-09-18 14:21:13 -0700

    add search field to staff accounts index

--- app/controllers/staff/accounts_controller.rb
+++ app/controllers/staff/accounts_controller.rb
@@ -26,19 +26,32 @@ class Staff::AccountsController < ApplicationController
     @locations = current_user.manageable_locations
     @programs = @locations.programs
 
-    @program = Program.find_by(id: params[:program_id]) || @center.current_program
-    @program = @programs.first unless @programs.include?(@program)
+    unless params[:search].blank?
+      @program_id = ""
+      @program = nil
+      @location_id = ""
+      @location = nil
 
-    @locations = Location.where(id: (@locations.pluck(:id) & @program.locations.pluck(:id)))
+      @accounts = Account.includes(:parents).joins(:user).where("lower(users.email) ILIKE ?", "%#{params[:search].to_s.downcase}%")
+    else
+      @program_id = params[:program_id]
+      @program = Program.find_by(id: @program_id)
+      @program = @programs.first if !current_user.super_admin? && !@programs.include?(@program)
+      @program_id = @program.id if @program.present?
 
-    @location_id = params[:location_id]
-    @location = Location.find_by(id: @location_id)
-    @location = @locations.first if !current_user.super_admin? && !@locations.include?(@location)
+      @locations = Location.where(id: (@locations.pluck(:id) & @program.locations.pluck(:id))) if @program.present?
 
-    @show_unregistered = params["show_unregistered"].present?
+      @location_id = params[:location_id]
+      @location = Location.find_by(id: @location_id)
+      @location = @locations.first if !current_user.super_admin? && !@locations.include?(@location)
+      @location_id = @location.id if @location.present?
 
-    @accounts = Account.includes(:parents).where(center: @center).by_program(@program).by_location(@location)
-    @accounts = @show_unregistered ? @accounts.where.not(signup_complete: true) : @accounts.where(signup_complete: true)
+      @show_unregistered = params["show_unregistered"].present?
+
+      @accounts = Account.includes(:parents).where(center: @center).by_program(@program).by_location(@location)
+      @accounts = @show_unregistered ? @accounts.where.not(signup_complete: true) : @accounts.where(signup_complete: true)
+    end
+
     @accounts = @accounts.distinct
   end
 end
