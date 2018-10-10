36eccb71a0e151a748386edf49731c9f4aeee798
Peter Cormack <petertcormack@gmail.com>
2018-10-10 09:20:46 -0700

    allow multiple camp day plans

--- app/models/target_day.rb
+++ app/models/target_day.rb
@@ -1,9 +1,7 @@
 class TargetDay < ApplicationRecord
   belongs_to :plan
 
-  def self.by_program_and_plan_type(program, plan_type)
-    joins(:plan).where("plans.program_id = ? AND plans.plan_type = ?", program.id, plan_type.to_s)
-  end
+  scope :by_program_and_plan_type, ->(program, plan_type) { joins(:plan).where("plans.program_id = ? AND plans.plan_type = ?", program.id, plan_type.to_s) }
 
   def self.select_options
     all.map{|target| [target.target_date.stamp("Tuesday, January 8th, 2018"), target.target_date.stamp("2018/03/08")]}
