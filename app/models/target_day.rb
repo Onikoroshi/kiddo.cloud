7c6d46708280f10b6467b7416265967ab258c6ca
Peter Cormack <petertcormack@gmail.com>
2018-10-31 12:21:29 -0700

    handle camp days for removed days

--- app/models/target_day.rb
+++ app/models/target_day.rb
@@ -4,6 +4,6 @@ class TargetDay < ApplicationRecord
   scope :by_program_and_plan_type, ->(program, plan_type) { joins(:plan).where("plans.program_id = ? AND plans.plan_type = ?", program.id, plan_type.to_s) }
 
   def self.select_options
-    all.map{|target| [target.target_date.stamp("Tuesday, January 8th, 2018"), target.target_date.stamp("2018/03/08")]}
+    all.sort{|a, b| a.target_date <=> b.target_date}.map{|target| [target.target_date.stamp("Tuesday, January 8th, 2018"), target.target_date.stamp("2018/03/08")]}
   end
 end
