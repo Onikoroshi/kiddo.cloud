f01c1473e1be12c75574426961f15e3135995bf0
Peter Cormack <petertcormack@gmail.com>
2019-08-20 17:45:58 -0700

    reorder programs on account dashboards

--- app/models/program.rb
+++ app/models/program.rb
@@ -34,6 +34,7 @@ class Program < ApplicationRecord
   after_save :sync_enrollments
 
   scope :order_by_priority, -> { order("priority ASC") }
+  scope :descending_by_start_date, -> { order("starts_at DESC") }
 
   scope :open_for_registration, -> { where("registration_opens <= ? AND registration_closes >= ?", Time.zone.today, Time.zone.today) }
   scope :in_session, -> { where("starts_at <= ? AND ends_at >= ?", Time.zone.today, Time.zone.today) }
