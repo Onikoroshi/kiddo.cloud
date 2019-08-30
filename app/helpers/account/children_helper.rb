c7897cf6af072ca3033b0a7bc2c31fe692825beb
Peter Cormack <petertcormack@gmail.com>
2019-08-30 14:38:52 -0700

    dynamically lock out unavailable locations

--- app/helpers/account/children_helper.rb
+++ app/helpers/account/children_helper.rb
@@ -2,4 +2,8 @@ module Account::ChildrenHelper
   def display_disallowed_grade_message
     "Please sign this child up for a program that serves their age group."
   end
+
+  def display_unavailable_locations_message
+    "Unfortunately, there are no locations currently available. Please contact the office to be put on the waitlist."
+  end
 end
