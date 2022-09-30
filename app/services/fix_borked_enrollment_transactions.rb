1e5845d997c6c252b04bf07666c657feb0808ad1
Peter Cormack <petertcormack@gmail.com>
2022-09-30 16:36:08 -0700

    add service to fix enrollment transactions

---
+++ app/services/fix_borked_enrollment_transactions.rb
@@ -0,0 +1,26 @@
+class FixBorkedEnrollmentTransactions
+  def fix_everything!
+    program = Program.find(45)
+    enrollments = Enrollment.by_program(program)
+    ap "looking at #{enrollments} enrollments"
+    enrollment = enrollments.to_a.sample
+    ap "examining enrollment #{enrollment.id} for account #{enrollment.account.id}"
+    ap "payment hash before:"
+    ap enrollment.payment_plan_hash
+
+    program_start_date = enrollment.program.starts_at.to_date
+
+    enrollment.enrollment_transactions.find_each do |enroll_trans|
+      replace = enroll_trans.description_data
+      start_date = replace["start_date"].to_date
+
+      replace["stop_date"] = start_date < program_start_date ? program_start_date - 1.day : start_date.end_of_month
+
+      enroll_trans.update_column(:description_data, replace)
+    end
+
+    enrollment.set_next_target_and_payment_date!
+    ap "payment hash after:"
+    ap enrollment.payment_plan_hash
+  end
+end
